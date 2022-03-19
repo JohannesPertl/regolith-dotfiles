9c7f18c3d7f0cacd068a17e9eed3f32b913f51fb
Johannes <johannes.pertl@protonmail.com>
2022-03-19 16:20:06 +0100

    Add android studio and KVM setup

--- setup-regolith.sh
+++ setup-regolith.sh
@@ -117,6 +117,9 @@ sudo snap install code --classic
 sudo snap install intellij-idea-ultimate --classic
 sudo snap install pycharm-professional --classic
 sudo snap install webstorm --classic
+sudo snap install android-studio --classic
+# Install KVM for accelerating emulator
+sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils -y
 }
 
 setup_grub_customizer(){
