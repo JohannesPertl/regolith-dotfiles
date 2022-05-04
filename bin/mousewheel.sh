bcc3e9750256a3c5339a90d16353eb2671d2df7d
Johannes Pertl <johannes.pertl@protonmail.com>
2022-05-04 16:50:00 +0200

    Adapt mousewheel.sh to set speed via param

--- bin/mousewheel.sh
+++ bin/mousewheel.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-# Version 0.1 Tuesday, 07 May 2013
-# Comments and complaints http://www.nicknorton.net
-# GUI for mouse wheel speed using imwheel in Gnome
-# imwheel needs to be installed for this script to work
-# sudo apt-get install imwheel
-# Pretty much hard wired to only use a mouse with
-# left, right and wheel in the middle.
-# If you have a mouse with complications or special needs,
-# use the command xev to find what your wheel does.
-#
+## Base on http://www.nicknorton.net/mousewheel.sh
 ### see if imwheel config exists, if not create it ###
 if [ ! -f ~/.imwheelrc ]
 then
@@ -28,14 +19,17 @@ fi
 
 CURRENT_VALUE=$(awk -F 'Button4,' '{print $2}' ~/.imwheelrc)
 
-NEW_VALUE=$(zenity --scale --window-icon=info --ok-label=Apply --title="Wheelies" --text "Mouse wheel speed:" --min-value=1 --max-value=100 --value="$CURRENT_VALUE" --step 1)
+NEW_VALUE=$1
 
-if [ "$NEW_VALUE" == "" ];
-then exit 0
+if [ -z "$NEW_VALUE" ]
+then
+    echo "Usage: $0 <speed>"
+    exit 1
 fi
 
 sed -i "s/\($TARGET_KEY *Button4, *\).*/\1$NEW_VALUE/" ~/.imwheelrc # find the string Button4, and write new value.
 sed -i "s/\($TARGET_KEY *Button5, *\).*/\1$NEW_VALUE/" ~/.imwheelrc # find the string Button5, and write new value.
 
 cat ~/.imwheelrc
-sudo imwheel --kill ---buttons "4 5"
+imwheel --kill --buttons "4 5"
+
