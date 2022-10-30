6bfe8e7838493f20370c249459bc6bc998c3c93e
Johannes Pertl <johannes.pertl@proton.me>
2022-10-30 13:32:24 +0100

    Update installing Signal

--- setup-regolith.sh
+++ setup-regolith.sh
@@ -177,9 +177,14 @@ setup_xpointerbarrier() {
 }
 
 setup_user_apps() {
-  sudo snap install signal-desktop
   sudo snap install discord
   sudo snap install vlc
+  # Signal
+  wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null &&
+	echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list &&
+  sudo apt update && sudo apt install signal-desktop
 }
 
 setup_laptop() {
